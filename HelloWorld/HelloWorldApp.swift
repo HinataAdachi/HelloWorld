67d64ebf2cf709b12c5ac4353f3aa6c445448db4
HinataAdachi <h.a.rakutano.001@gmail.com>
2023-07-18 14:59:44 +0900

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 足立日向 on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
